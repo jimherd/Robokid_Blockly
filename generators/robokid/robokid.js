f476cec0685749c01a84c3699db30a89b65dad60
Jim Herd <jtherd@gmail.com>
2012-07-30 19:36:51 +0100

    Update to Blockly r376

--- generators/robokid/robokid.js
+++ generators/robokid/robokid.js
@@ -28,8 +28,67 @@ Blockly.Robokid = Blockly.Generator.get('Robokid');
 
 
 Blockly.Robokid.LEDS_set = function() {
-  // 
-  return 'LEDS_set' + '\n';
+  // leds command for 4 LEDs on the Robokid robot
+  // Todo : posible use of an array rather than individual variables
+  var LED_A_value, LED_B_value, LED_C_value, LED_D_value;
+    switch (this.getTitleValue('LED_A_mode')) {
+		case 'off':
+			LED_A_value = '0';
+			break;
+		case 'on':
+			LED_A_value = '1';
+			break;
+		case 'flash':
+			LED_A_value = '2';
+			break;
+		case 'nochange':
+			LED_A_value = '3';
+			break;
+  }
+    switch (this.getTitleValue('LED_B_mode')) {
+		case 'off':
+			LED_B_value = '0';
+			break;
+		case 'on':
+			LED_B_value = '1';
+			break;
+		case 'flash':
+			LED_B_value = '2';
+			break;
+		case 'nochange':
+			LED_B_value = '3';
+			break;
+  }
+    switch (this.getTitleValue('LED_C_mode')) {
+		case 'off':
+			LED_C_value = '0';
+			break;
+		case 'on':
+			LED_C_value = '1';
+			break;
+		case 'flash':
+			LED_C_value = '2';
+			break;
+		case 'nochange':
+			LED_C_value = '3';
+			break;
+  }
+    switch (this.getTitleValue('LED_D_mode')) {
+		case 'off':
+			LED_D_value = '0';
+			break;
+		case 'on':
+			LED_D_value = '1';
+			break;
+		case 'flash':
+			LED_D_value = '2';
+			break;
+		case 'nochange':
+			LED_D_value = '3';
+			break;
+  }
+  return 'leds ' + LED_A_value  + ' ' + LED_B_value + ' ' + LED_C_value + 
+			' ' + LED_D_value +';\n';
 };
 
 Blockly.Robokid.motors_set = function() {
@@ -60,15 +119,15 @@ Blockly.Robokid.ubasic_for = function() {
   var branch0 = Blockly.Robokid.statementToCode(this, 'DO');
   var code;
   if (argument1.match(/^\w+$/)) {
-    code = '0 for ' + variable0 + '=' + argument0 + ' to ' + argument1 + 
-        '\n' + branch0 + '0 next ' + variable0 + '\n';
+    code = 'for ' + variable0 + '=' + argument0 + ' to ' + argument1 + 
+        '\n' + branch0 + '};' + '\n';
   } else {
     // The end value appears to be more complicated than a simple variable.
     // Cache it to a variable to prevent repeated look-ups.
     var endVar = Blockly.Robokid.variableDB_.getDistinctName(
         variable0 + '_end', Blockly.Variables.NAME_TYPE);
     code = 'var ' + endVar + ' = ' + argument1 + ';\n' +
-        'for (' + variable0 + ' = ' + argument0 + '; ' +
+        'for ' + variable0 + ' = ' + argument0 + ' to ' +
               variable0 + ' <= ' + endVar + '; ' +
               variable0 + '++) {\n' +
         branch0 + '}\n';
