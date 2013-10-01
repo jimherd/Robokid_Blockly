e9fc01cd1347cfe4c88280d83cb601da9a311186
Jim Herd <jtherd@gmail.com>
2013-10-01 22:29:58 +0100

    To r1393

--- apps/Robokid_code/generated/en.js
+++ apps/Robokid_code/generated/en.js
@@ -1,3 +1,13 @@
 //
 // dummy en.js file
 //
+
+if (typeof apps == 'undefined') { var apps = {}; }
+
+apps.storageDialog = function(opt_data, opt_ignored, opt_ijData) {
+  return '<div id="dialogStorage" class="dialogHiddenContent"><div id="containerStorage"></div>' + apps.ok(null, null, opt_ijData) + '</div>';
+};
+
+apps.ok = function(opt_data, opt_ignored, opt_ijData) {
+  return '<div class="farSide" style="padding: 1ex 3ex 0"><button class="secondary" onclick="BlocklyApps.hideDialog(true)">OK</button></div>';
+};
